710ad8df7cb4ed96228ae683b5930c5903e93351
Elizabeth Woodhouse <elizabethjpwoodhouse@gmail.com>
2021-01-22 15:28:02 -0500

    finished setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require ('path');
+const layout= require ('./views/layout');
 const staticMiddleware=express.static(path.join(__dirname,'/public'));
 
 const app = express ();
@@ -9,7 +10,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 
 app.get("/",(req,res) =>{
-    res.send('hello world');
+    res.send(layout('hello world'));
 })
 
 
